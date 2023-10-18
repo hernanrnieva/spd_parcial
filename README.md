1a8f848665464e2c4f0ac3806cb24d11ad5f9ee1
hernanrnieva <nieva.hr@gmail.com>
2023-10-18 20:06:28 -0300

    actualización código y README

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 
 ## Integrantes 
-- Lionel Prats
+- Nicolas Peralta
 - Hernan Nieva
 
 
@@ -16,32 +16,94 @@ En este proyecto se implementa un contador binario haciendo uso de dos displays 
 Se usa la técnica de multiplexado para no necesitar más pines de los que ofrece Arduino.
 
 ## Loop principal
-El loop standard de arduino recibe las distintas pulsaciones a partir de la función press() (explicada en mayor profundidad con cometarios en el código).
-
-En base a la recepción de este valor, aumenta, disminuye, o resetea la cuenta, que luego es impresa por la función printCount():
+El loop standard de arduino recibe las distintas pulsaciones a partir de la función teclaPresionada().
+En base a la recepción de este valor, aumenta, disminuye, o resetea la cuenta, que luego es impresa por la función controladorDisplay():
 
 ~~~ C++
-void loop() {
-  int pressed = press();
+void loop()
+{      
+  // Ejecucion del antirrebote
+  int presionoTecla = teclaPresionada();
+
+  // Acción sobre el pulsador en base a tecla presionada
+  if(presionoTecla == SUBE)
+  {
+  	contadorNumeros += 1;
+   
+    if(contadorNumeros > 99)
+    {
+      contadorNumeros = 0;
+    }
+  }
+  else if(presionoTecla == BAJA)
+  {
+    contadorNumeros -= 1;
+
+    if(contadorNumeros < 0)
+    {
+      contadorNumeros = 99;
+    }
+  }
+  else if(presionoTecla == RESET)
+  {
+     contadorNumeros = 0;
+  }
   
-  if(pressed == UP) {
-    countDigit ++;
+  // Llamamos a la funcion para el manejo de los display
+  controladorDisplay(contadorNumeros);
+}
+~~~
 
-    if(countDigit > 99)
-      countDigit = 0;
+~~~ C++
+La función teclaPresionada() está hecha de tal forma que se evite el rebote del pulsador, es decir, que el mantener apretado el botón no se detecte como varias pulsaciones seguidas.
+Se verifica el estado anterior del pulsador para sólo devolver cuando estuvo presionado y dejó de estarlo:
 
-  } else if(pressed == DOWN) {
-    countDigit --;
+int teclaPresionada(void)
+{
+  // Función para detectar la pulsación de botones y gestionar el antirrebote.
+  // Devuelve SUBE, BAJA o RESET en caso de pulsación o 0 en caso contrario.
+  leerReiniciar = digitalRead(RESET);
+  leerSumar = digitalRead(SUBE);
+  leerRestar = digitalRead(BAJA);
+  
+  if(leerSumar == 1)
+  {
+    sumarAnterior = 1;
+  }
+  
+  if(leerRestar  == 1)
+  {
+    restarAnterior = 1;
+  }
 
-    if(countDigit < 0)
-      countDigit = 99;
+  if(leerReiniciar  == 1)
+  {
+    reiniciarAnterior = 1;
+  }
+  
+  // ANTIRREBOTE 
+  if(leerSumar == 0 && leerSumar != sumarAnterior)
+  {
+  	sumarAnterior = leerSumar;
+    return SUBE;
+  }
+  
+  if(leerRestar == 0 && leerRestar != restarAnterior)
+  {
+  	restarAnterior = leerRestar;
+    return BAJA;
+  }
+  
+  if(leerReiniciar == 0 && leerReiniciar != reiniciarAnterior)
+  {
+  	reiniciarAnterior = leerReiniciar;
+    return RESET;
+  }
 
-  } else if(pressed == RESET)
-    countDigit = 0;
-
-  printCount(countDigit);
+  // Devuelve 0 en caso de que no pase nada de lo anterior
+  return 0;
 }
 ~~~
 
 ## :robot: Link al proyecto en Tinkercad
-- [proyecto](https://www.tinkercad.com/things/358hSCJG9fT)
+- [proyecto](https://www.tinkercad.com/things/3Hq4TQANRCd)
--- code/arduino.cpp
+++ code/arduino.cpp
@@ -1,247 +1,226 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-/*
- * Dummy variables and functions to use IDE without undeclared variable errors.
- * Variables y funciones sin uso para poder usar IDW sin errores de variables no declaradas.
-*/
-#define INPUT_PULLUP 0
-#define OUTPUT 0
-#define A4 0
-#define A5 0
-#define LOW 0
-#define HIGH 0
-int digitalRead(int pin) {
-  return 0; 
-}
-void digitalWrite(int pin, int value) {
-  return; 
-}
-void pinMode(int pin, int mode) {
-  return; 
-}
-void delay(int secs) {
-  return;
-}
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-/* Código Arduino */
-
-// Constantes
+// Constantes 
 #define A 12
 #define B 13
-#define C 7
+#define C 9
 #define D 8
-#define E 9
+#define E 7
 #define F 11
 #define G 10
-#define UP 5
-#define DOWN 4
-#define RESET 3
-#define UNITS A4
-#define TENS A5
-#define MS_DISPLAYON 25
+#define SUBE 2
+#define BAJA 3
+#define RESET 4
+#define DISPLAY_DELAY 25
 
-// Variables globales
-int countDigit = 0;
-int up = 1;
-int previousUp = 1;
-int down = 1;
-int previousDown = 1;
-int reset = 1;
-int previousReset = 1;
-
-// Setup
-void setup () {
-  // Inicializo pines
-  for(int i = 3; i < 14; i++) {
-    if(i < 7)
-      pinMode(i, INPUT_PULLUP);
-    else
-      pinMode(i, OUTPUT);
+// Configuración de pines como entradas o salidas
+void setup()
+{
+  for(int i = 7; i < 14; i++) {
+    pinMode(i, OUTPUT);
   }
-
-  pinMode(UNITS, OUTPUT);
-  pinMode(TENS, OUTPUT);
-  
-  // Muestro 0 en ambos displays
-  digitalWrite(UNITS, 0);
-  digitalWrite(TENS, 0);
-  printDigit(0);
+  pinMode(A4, OUTPUT);            // Display 1
+  pinMode(A5, OUTPUT);			      // Display 2
+   
+  pinMode(RESET, INPUT_PULLUP);		// Pulsador de reinicio
+  pinMode(BAJA, INPUT_PULLUP);		// Pulsador -
+  pinMode(SUBE, INPUT_PULLUP);		// Pulsador +
 }
 
-void loop() {
-  int pressed = press();
+// Inicializacion de variables para utilizar en el codigo
+// A utilizar en el estado de los botones y manejar el antirrebote
+int contadorNumeros = 0;
+int leerReiniciar = 0;
+int leerSumar = 0;
+int leerRestar = 0;
+int restarAnterior = 1;
+int sumarAnterior = 1;
+int reiniciarAnterior = 1;
   
-  if(pressed == UP) {
-    countDigit ++;
+void loop()
+{      
+  // Ejecucion del antirrebote
+  int presionoTecla = teclaPresionada();
 
-    if(countDigit > 99)
-      countDigit = 0;
-
-  } else if(pressed == DOWN) {
-    countDigit --;
-
-    if(countDigit < 0)
-      countDigit = 99;
-
-  } else if(pressed == RESET)
-    countDigit = 0;
-
-  printCount(countDigit);
-}
-
-/*
- * Lee diferencias entre estado anterior de un botón y posterior.
- * Establece una presionada cuando se suelta un botón previamente presionado,
- * y devuelve el valor correspondiente al botón.
-*/
-int press() {
-  // Leo todos los estados actuales.
-  up = digitalRead(UP);
-  down = digitalRead(DOWN);
-  reset = digitalRead(RESET);
-  
-  // De no estar presionados, asumo el estado previo como no presionados.
-  if(up == 1)
-    previousUp = 1;
-  if(down == 1)
-    previousDown = 1;
-  if(reset == 1)
-    previousReset = 1;
-  
-  // En la primera condición verifico que no esté presionado y que previamente lo haya estado.
-  if(!up && up != previousUp) {
-    // Seteo el estado anterior como presionado para evitar entrar nuevamente en la condición si se mantiene presionado.
-    previousUp = up;
-    return UP;
-  }
-  
-  // Repito la lógica para los otros dos botones.
-  if(!down && down != previousDown) {
-    previousDown = down;
-    return DOWN;
-  }
-  
-  if(!reset && reset != previousReset) {
-    previousReset = reset;
-    return RESET;
-  }
-  
-  // Devuelvo un 0 genérico que hace que no se modifique el valor de count.
-  return 0;
-}
-
-void printDigit(int digit) {
-  // Apago todos lo segmentos.
-  digitalWrite(A, LOW);
-  digitalWrite(B, LOW);
-  digitalWrite(C, LOW);
-  digitalWrite(D, LOW);
-  digitalWrite(E, LOW);
-  digitalWrite(F, LOW);
-  digitalWrite(G, LOW);
-
-  // Prendo los segmentos acorde a cada número.
-  switch(digit) {
-    case 1: {
-      digitalWrite(B, HIGH);
-      digitalWrite(C, HIGH);
-      break;
-    } case 2: {
-      digitalWrite(A, HIGH);
-      digitalWrite(B, HIGH);
-      digitalWrite(D, HIGH);
-      digitalWrite(E, HIGH);
-      digitalWrite(G, HIGH);
-      break;
-    } case 3: {
-      digitalWrite(A, HIGH);
-      digitalWrite(B, HIGH);
-      digitalWrite(C, HIGH);
-      digitalWrite(D, HIGH);
-      digitalWrite(G, HIGH);
-      break;
-    } case 4: {
-      digitalWrite(B, HIGH);
-      digitalWrite(C, HIGH);
-      digitalWrite(F, HIGH);
-      digitalWrite(G, HIGH);
-      break;
-    } case 5: {
-      digitalWrite(A, HIGH);
-      digitalWrite(C, HIGH);
-      digitalWrite(D, HIGH);
-      digitalWrite(F, HIGH);
-      digitalWrite(G, HIGH);
-      break;
-    } case 6: {
-      digitalWrite(A, HIGH);
-      digitalWrite(C, HIGH);
-      digitalWrite(D, HIGH);
-      digitalWrite(E, HIGH);
-      digitalWrite(F, HIGH);
-      digitalWrite(G, HIGH);
-      break;
-    } case 7: {
-      digitalWrite(A, HIGH);
-      digitalWrite(B, HIGH);
-      digitalWrite(C, HIGH);
-      break;
-    } case 8: {
-      digitalWrite(A, HIGH);
-      digitalWrite(B, HIGH);
-      digitalWrite(C, HIGH);
-      digitalWrite(D, HIGH);
-      digitalWrite(E, HIGH);
-      digitalWrite(F, HIGH);
-      digitalWrite(G, HIGH);
-      break;
-    } case 9: {
-      digitalWrite(A, HIGH);
-      digitalWrite(B, HIGH);
-      digitalWrite(C, HIGH);
-      digitalWrite(D, HIGH);
-      digitalWrite(F, HIGH);
-      digitalWrite(G, HIGH);
-      break;
-    } case 0: {
-      digitalWrite(A, HIGH);
-      digitalWrite(B, HIGH);
-      digitalWrite(C, HIGH);
-      digitalWrite(D, HIGH);
-      digitalWrite(E, HIGH);
-      digitalWrite(F, HIGH);
-      break;
+  // Acción sobre el pulsador en base a tecla presionada
+  if(presionoTecla == SUBE)
+  {
+  	contadorNumeros += 1;
+   
+    if(contadorNumeros > 99)
+    {
+      contadorNumeros = 0;
     }
   }
+  else if(presionoTecla == BAJA)
+  {
+    contadorNumeros -= 1;
+
+    if(contadorNumeros < 0)
+    {
+      contadorNumeros = 99;
+    }
+  }
+  else if(presionoTecla == RESET)
+  {
+     contadorNumeros = 0;
+  }
+  
+  // Llamamos a la funcion para el manejo de los display
+  controladorDisplay(contadorNumeros);
 }
 
-void turnDigit(int digit) {
-  // Enciendo las unidades por el tiempo de display establecido.
-  if(digit == UNITS) {
-    digitalWrite(UNITS, LOW);
-    digitalWrite(TENS, HIGH);
-  	delay(MS_DISPLAYON);
-  // Enciendo las decenas por el tiempo de display establecido.
-  } else if(digit == TENS) {
-    digitalWrite(UNITS, HIGH);
-    digitalWrite(TENS, LOW);
-  	delay(MS_DISPLAYON);
-  // Enciendo ambas indefinidamente.
-  } else {   
-    digitalWrite(UNITS, LOW);
-    digitalWrite(TENS, LOW);
+// Control del funcionamiento de los display
+void controladorDisplay(int contadorNumeros)
+{
+  // Sacar las unidades y decenas del contador
+  int unidades = contadorNumeros % 10;
+  int decenas = contadorNumeros / 10;
+
+  // Mostrar las unidades en el primer display
+  numerosDisplay(unidades);
+  prenderDisplay(LOW, HIGH);
+  delay(DISPLAY_DELAY); 
+
+  // Mostrar las decenas en el segundo display
+  numerosDisplay(decenas);
+  prenderDisplay(HIGH, LOW);
+  delay(DISPLAY_DELAY); 
+}
+
+// Funcion para mostrar los numeros en el display
+void numerosDisplay(int numero){
+  borrar_display();
+      
+  switch(numero) {
+  	case 0:
+      digitalWrite(A, HIGH); 
+      digitalWrite(B, HIGH); 
+      digitalWrite(C, HIGH); 
+      digitalWrite(D, HIGH); 
+      digitalWrite(E, HIGH); 
+      digitalWrite(F, HIGH); 
+      break;
+  	case 1: 
+      digitalWrite(B, HIGH); 
+      digitalWrite(C, HIGH); 
+    	break;
+    case 2: 
+      digitalWrite(A, HIGH);
+    	digitalWrite(B, HIGH); 
+      digitalWrite(G, HIGH); 
+    	digitalWrite(E, HIGH); 
+    	digitalWrite(D, HIGH); 
+    	break;
+    case 3: 
+      digitalWrite(A, HIGH);
+    	digitalWrite(B, HIGH); 
+      digitalWrite(G, HIGH); 
+    	digitalWrite(C, HIGH); 
+    	digitalWrite(D, HIGH); 
+    	break;
+    case 4: 
+      digitalWrite(F, HIGH);
+    	digitalWrite(B, HIGH); 
+      digitalWrite(G, HIGH); 
+    	digitalWrite(C, HIGH); 
+    	break;
+    case 5: 
+      digitalWrite(A, HIGH);
+    	digitalWrite(F, HIGH); 
+      digitalWrite(G, HIGH); 
+    	digitalWrite(C, HIGH);
+    	digitalWrite(D, HIGH);
+    	break;
+    case 6: 
+      digitalWrite(A, HIGH);
+    	digitalWrite(F, HIGH); 
+      digitalWrite(G, HIGH); 
+    	digitalWrite(C, HIGH);
+    	digitalWrite(E, HIGH);
+    	digitalWrite(D, HIGH);
+    	break;
+    case 7: 
+      digitalWrite(A, HIGH);
+    	digitalWrite(B, HIGH); 
+    	digitalWrite(C, HIGH);
+    	break;
+    case 8: 
+      digitalWrite(A, HIGH); 
+      digitalWrite(B, HIGH); 
+      digitalWrite(C, HIGH); 
+      digitalWrite(D, HIGH); 
+      digitalWrite(E, HIGH); 
+      digitalWrite(F, HIGH);
+    	digitalWrite(G, HIGH);
+    	break;
+    case 9: 
+      digitalWrite(A, HIGH); 
+      digitalWrite(B, HIGH); 
+      digitalWrite(C, HIGH);  
+      digitalWrite(F, HIGH);
+    	digitalWrite(G, HIGH);
+    	break;
   }
 }
 
-void printCount(int count) {
-  turnDigit(0);
-  // Prendo los segmentos pertenecientes a las decenas.
-  printDigit(count/10);
-  // Los envío al display de decenas.
-  turnDigit(TENS);
-  turnDigit(0);
-  // Prendo los segmentos pertenecientes a las unidades.
-  printDigit(count - 10*((int)count/10));
-  // Los envío al display de unidades.
-  turnDigit(UNITS);
+// Apaga todos los segmentos del display
+void borrar_display(){
+  digitalWrite(A, LOW); 
+	digitalWrite(B, LOW); 
+	digitalWrite(C, LOW); 
+	digitalWrite(D, LOW); 
+	digitalWrite(E, LOW); 
+	digitalWrite(F, LOW); 
+	digitalWrite(G, LOW); 
+}
+
+// Funcion que controla el encendido de los display
+void prenderDisplay(int display1, int display2){
+  digitalWrite(A5, display1);
+  digitalWrite(A4, display2);
+}
+
+int teclaPresionada(void)
+{
+  // Función para detectar la pulsación de botones y gestionar el antirrebote.
+  // Devuelve SUBE, BAJA o RESET en caso de pulsación o 0 en caso contrario.
+  leerReiniciar = digitalRead(RESET);
+  leerSumar = digitalRead(SUBE);
+  leerRestar = digitalRead(BAJA);
+  
+  if(leerSumar == 1)
+  {
+    sumarAnterior = 1;
+  }
+  
+  if(leerRestar  == 1)
+  {
+    restarAnterior = 1;
+  }
+
+  if(leerReiniciar  == 1)
+  {
+    reiniciarAnterior = 1;
+  }
+  
+  // ANTIRREBOTE 
+  if(leerSumar == 0 && leerSumar != sumarAnterior)
+  {
+  	sumarAnterior = leerSumar;
+    return SUBE;
+  }
+  
+  if(leerRestar == 0 && leerRestar != restarAnterior)
+  {
+  	restarAnterior = leerRestar;
+    return BAJA;
+  }
+  
+  if(leerReiniciar == 0 && leerReiniciar != reiniciarAnterior)
+  {
+  	reiniciarAnterior = leerReiniciar;
+    return RESET;
+  }
+
+  // Devuelve 0 en caso de que no pase nada de lo anterior
+  return 0;
 }
